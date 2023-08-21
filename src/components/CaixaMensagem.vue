c26a712f024b012810ca14dd7ba5f2eec24bac0d
waltereidi <walter-eidi@hotmail.com>
2023-08-21 18:08:54 -0300

    caixa de mensagem para bibliotecaonline

---
+++ src/components/CaixaMensagem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>caixaMensagem</h1>
+
+    
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import CaixaMensagem from '@/components/CaixaMensagem.vue'
+
+createApp(CaixaMensagem).mount('#app')
